0c95238219eee8a6f38e910168d8f6a54ec00a91
manjeet2k <manjeet.1996.12@gmail.com>
2020-06-02 13:05:40 +0530

    Added Flash Company Model Homepage Navbar Bootstrap

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,57 @@
+class CompaniesController < ApplicationController
+  
+  def index
+    @company = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    company = Company.new(comp_params)
+    
+    if company.save
+      flash[:success] = "Company was succesfully created!"
+      redirect_to "/companies"
+    else
+      flash[:warning] = "#{company.errors.full_messages}"
+      redirect_to "/companies/new"
+    end
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def show
+    @company = Company.find(params[:id])
+    redirect_to "/companies/#{@company.id}/edit"
+  end
+
+  def update
+    company = Company.find(params[:id])
+    
+    if company.update(comp_params)
+      flash[:success] = "Company was succesfully Updated!"
+      redirect_to "/companies"
+    else
+      flash[:warning] = "#{company.errors.full_messages}"
+      redirect_to "/companies/#{company.id}/edit"
+    end
+  end
+
+  def destroy
+    company = Company.find(params[:id])
+    company.delete
+    flash[:danger] = " Company #{company.name} was deleted."
+    redirect_to "/companies"
+  end
+
+  private
+  
+  def comp_params
+    params.require(:company).permit(:name)
+  end
+  
+end
